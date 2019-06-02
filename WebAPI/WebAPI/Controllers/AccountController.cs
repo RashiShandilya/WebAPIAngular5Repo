55d2bd8c8368cf9937cd652ed8b89c8df6c48cf4
Rashi Shandiliya <shandiliya.rashi@gmail.com>
2019-06-02 21:50:48 +1200

    first commit

---
+++ WebAPI/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+       
+
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IHttpActionResult Register(AccountModel model)
+        {
+           
+            var user = new User() {
+                UserName = model.UserName,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Password = model.Password
+            };
+
+            var result = _service.Create(model);
+           
+                       
+            return result.Success
+                ? Json(new { Message = "Ok" })
+                : Json(new { Message = "Error" });
+        }
+    }
+}
